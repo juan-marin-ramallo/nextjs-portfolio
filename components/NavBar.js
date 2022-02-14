afa3c3d643ab2d2a7c944c2c42cf91e42a29b2f0
Juan Marin <jmarin@espol.edu.ec>
2022-02-14 01:06:54 -0500

    Change

--- components/NavBar.js
+++ components/NavBar.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 const NavBar = () => (
   <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container">
-    <Link href="/">
+    <Link href="/nextjs-portfolio/">
         <a className="navbar-brand">Personal Portfolio</a>
     </Link>    
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" 
